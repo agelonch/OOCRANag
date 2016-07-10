5cf4f54d5b84ca4dc4a63fa374eb75ff03365898
howls <mfloriach90@gmail.com>
2016-07-10 23:11:28 +0200

    proves

--- deployments/cron.py
+++ deployments/cron.py
@@ -13,56 +13,57 @@ from aloeoCLI.VNFM.deployments.deployments import create
 from .authentication import auth
 from .orchestration import planification_DL, planification_UL, price
 
-def my_scheduled_job():
-  deploy = get_object_or_404(Deployment, stop=time.strftime("%H:00:00"))
-  nvfs = Nvf.objects.filter(deploy__name=deploy.name)
-  for nvf in nvfs:
-		bts = get_object_or_404(Bts, ip=nvf.bts.ip)
-		lista = bts.freCs.split('/')
-		lista.remove(str(nvf.freC_DL-nvf.BW_DL/2)+"-"+str(nvf.freC_DL+nvf.BW_DL/2))
-		lista.remove(str(nvf.freC_UL-nvf.BW_UL/2)+"-"+str(nvf.freC_UL+nvf.BW_UL/2))
-		bts.freCs= '/'.join(lista)
-		bts.save()
 
-  deploy.delete()
+def my_scheduled_job():
+    deploy = get_object_or_404(Deployment, stop=time.strftime("%H:00:00"))
+    nvfs = Nvf.objects.filter(deploy__name=deploy.name)
+    for nvf in nvfs:
+        bts = get_object_or_404(Bts, ip=nvf.bts.ip)
+        lista = bts.freCs.split('/')
+        lista.remove(str(nvf.freC_DL - nvf.BW_DL / 2) + "-" + str(nvf.freC_DL + nvf.BW_DL / 2))
+        lista.remove(str(nvf.freC_UL - nvf.BW_UL / 2) + "-" + str(nvf.freC_UL + nvf.BW_UL / 2))
+        bts.freCs = '/'.join(lista)
+        bts.save()
+
+    deploy.delete()
 
 
 def auto_delete_deploy_job():
-	deploys = Deployment.objects.filter(propietario__name="operator1").filter(start__isnull=False)
-	    
-	for deploy in deploys:
-		plan = str(deploy.area.rb_offer)[1:-1].split(',')
+    deploys = Deployment.objects.filter(propietario__name="operator1").filter(start__isnull=False)
 
-		nvfs = Nvf.objects.filter(deploy__name=plan[int(time.strftime("%H"))-1])
-        for nvf in nvfs:
-        	bts = get_object_or_404(Bts, ip = nvf.bts.ip)
-        	if len(bts.freCs) is 0:
-        		continue
-        	else:
-        		lista = bts.freCs.split('/')
-        		lista.remove(str(nvf.freC_DL-nvf.BW_DL/2)+"-"+str(nvf.freC_DL+nvf.BW_DL/2))
-        		lista.remove(str(nvf.freC_UL-nvf.BW_UL/2)+"-"+str(nvf.freC_UL+nvf.BW_UL/2))
-        		bts.save()
-        		nvf.freC_DL = 0
-        		nvf.freC_UL = 0
-        		nvf.radio = 0
-        		nvf.save()
-		
+    for deploy in deploys:
+        plan = str(deploy.area.rb_offer)[1:-1].split(',')
+
+        nvfs = Nvf.objects.filter(deploy__name=plan[int(time.strftime("%H")) - 1])
+    for nvf in nvfs:
+        bts = get_object_or_404(Bts, ip=nvf.bts.ip)
+        if len(bts.freCs) is 0:
+            continue
+        else:
+            lista = bts.freCs.split('/')
+            lista.remove(str(nvf.freC_DL - nvf.BW_DL / 2) + "-" + str(nvf.freC_DL + nvf.BW_DL / 2))
+            lista.remove(str(nvf.freC_UL - nvf.BW_UL / 2) + "-" + str(nvf.freC_UL + nvf.BW_UL / 2))
+            bts.save()
+            nvf.freC_DL = 0
+            nvf.freC_UL = 0
+            nvf.radio = 0
+            nvf.save()
+
 
 def auto_create_deploy_job():
-	deploys = Deployment.objects.filter(propietario__name="operator1").filter(start__isnull=False)
-	    
-	for deploy in deploys:
-		plan = str(deploy.area.rb_offer)[1:-1].split(',')
-		nvfs = Nvf.objects.filter(deploy__name=plan[int(time.strftime("%H"))])
-        for nvf in nvfs:
-	        bts = get_object_or_404(Bts, ip = nvf.bts.ip)
-	        bts.freCs = planification_DL(nvf, Bts.objects.filter(area__name="EETAC"))
-	        bts.save()
-	        bts.freCs = planification_UL(nvf, Bts.objects.all())
-	        nvf.radio = nvf.bts.max_dist(nvf.Pt,nvf.freC_DL)          
-	        deploy.rb += nvf.rb_offer
-	        deploy.price = deploy.price + price(nvf, nvf.BW_DL, deploy)
-	        bts.save()
-	        nvf.save()
-	        deploy.save()
+    deploys = Deployment.objects.filter(propietario__name="operator1").filter(start__isnull=False)
+
+    for deploy in deploys:
+        plan = str(deploy.area.rb_offer)[1:-1].split(',')
+        nvfs = Nvf.objects.filter(deploy__name=plan[int(time.strftime("%H"))])
+    for nvf in nvfs:
+        bts = get_object_or_404(Bts, ip=nvf.bts.ip)
+        bts.freCs = planification_DL(nvf, Bts.objects.filter(area__name="EETAC"))
+        bts.save()
+        bts.freCs = planification_UL(nvf, Bts.objects.all())
+        nvf.radio = nvf.bts.max_dist(nvf.Pt, nvf.freC_DL)
+        deploy.rb += nvf.rb_offer
+        deploy.price = deploy.price + price(nvf, nvf.BW_DL, deploy)
+        bts.save()
+        nvf.save()
+        deploy.save()
